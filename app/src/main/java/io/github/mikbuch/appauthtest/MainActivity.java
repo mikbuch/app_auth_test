bb9828a4586037d2d45731b3a35193e26c6a38c1
Mikolaj Buchwald <mikbuch@users.noreply.github.com>
2020-09-04 15:30:36 +0200

    Commiting working version of the application.

---
+++ app/src/main/java/io/github/mikbuch/appauthtest/MainActivity.java
@@ -0,0 +1,252 @@
+package io.github.mikbuch.appauthtest;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.TokenResponse;
+
+import org.json.JSONException;
+
+import static io.github.mikbuch.appauthtest.MainApplication.LOG_TAG;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREFERENCES_NAME = "AuthStatePreference";
+    private static final String AUTH_STATE = "AUTH_STATE";
+    private static final String USED_INTENT = "USED_INTENT";
+
+    MainApplication mMainApplication;
+
+    // state
+    AuthState mAuthState;
+
+    // views
+    Button mAuthorize;
+    Button mMakeApiCall;
+    Button mSignOut;
+    TextView mGivenName;
+    TextView mFamilyName;
+    TextView mFullName;
+    ImageView mProfileView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        mMainApplication = (MainApplication) getApplication();
+        mAuthorize = findViewById(R.id.authorize);
+        mMakeApiCall = findViewById(R.id.makeApiCall);
+        mSignOut = findViewById(R.id.signOut);
+        mGivenName = findViewById(R.id.givenName);
+        mFamilyName = findViewById(R.id.familyName);
+        mFullName = findViewById(R.id.fullName);
+        mProfileView = findViewById(R.id.profileImage);
+
+        enablePostAuthorizationFlows();
+
+        // wire click listeners
+        mAuthorize.setOnClickListener(new AuthorizeListener());
+    }
+
+    private void enablePostAuthorizationFlows() {
+        mAuthState = restoreAuthState();
+        if (mAuthState != null && mAuthState.isAuthorized()) {
+            if (mMakeApiCall.getVisibility() == View.GONE) {
+                mMakeApiCall.setVisibility(View.VISIBLE);
+                mMakeApiCall.setOnClickListener(new MakeApiCallListener(this, mAuthState, new AuthorizationService(this)));
+            }
+            if (mSignOut.getVisibility() == View.GONE) {
+                mSignOut.setVisibility(View.VISIBLE);
+                mSignOut.setOnClickListener(new SignOutListener(this));
+            }
+        } else {
+            mMakeApiCall.setVisibility(View.GONE);
+            mSignOut.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Exchanges the code, for the {@link TokenResponse}.
+     *
+     * @param intent represents the {@link Intent} from the Custom Tabs or the System Browser.
+     */
+    private void handleAuthorizationResponse(@NonNull Intent intent) {
+
+        // code from the step 'Handle the Authorization Response' goes here.
+
+        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+        AuthorizationException error = AuthorizationException.fromIntent(intent);
+        final AuthState authState = new AuthState(response, error);
+
+        if (response != null) {
+            Log.i(LOG_TAG, String.format("Handled Authorization Response %s ", authState.toJsonString()));
+            AuthorizationService service = new AuthorizationService(this);
+            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+                @Override
+                public void onTokenRequestCompleted(@Nullable TokenResponse tokenResponse, @Nullable AuthorizationException exception) {
+                    if (exception != null) {
+                        Log.w(LOG_TAG, "Token Exchange failed", exception);
+                    } else {
+                        if (tokenResponse != null) {
+                            authState.update(tokenResponse, exception);
+                            persistAuthState(authState);
+                            Log.i(LOG_TAG, String.format("Token Response [ Access Token: %s, ID Token: %s ]", tokenResponse.accessToken, tokenResponse.idToken));
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void persistAuthState(@NonNull AuthState authState) {
+        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE).edit()
+                .putString(AUTH_STATE, authState.toJsonString())
+                .commit();
+        enablePostAuthorizationFlows();
+    }
+
+    private void clearAuthState() {
+        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+                .edit()
+                .remove(AUTH_STATE)
+                .apply();
+    }
+
+    @Nullable
+    private AuthState restoreAuthState() {
+        String jsonString = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+                .getString(AUTH_STATE, null);
+        if (!TextUtils.isEmpty(jsonString)) {
+            try {
+                return AuthState.fromJson(jsonString);
+            } catch (JSONException jsonException) {
+                // should never happen
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Kicks off the authorization flow.
+     */
+    public static class AuthorizeListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            // code from the step 'Create the Authorization Request',
+            // and the step 'Perform the Authorization Request' goes here.
+
+            AuthorizationServiceConfiguration serviceConfiguration = new AuthorizationServiceConfiguration(
+                    Uri.parse("https://accounts.google.com/o/oauth2/v2/auth") /* auth endpoint */,
+                    Uri.parse("https://www.googleapis.com/oauth2/v4/token") /* token endpoint */
+            );
+
+            // Setup Client ID in local.properties file.
+            // Example Client ID: 569458729478-g18o2rojvs5a8eh9nk41f6v42mpraq9k.apps.googleusercontent.com
+            String clientId = BuildConfig.CLIENT_ID;
+            Uri redirectUri = Uri.parse("com.google.codelabs.appauth:/oauth2callback");
+            AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
+                    serviceConfiguration,
+                    clientId,
+                    AuthorizationRequest.RESPONSE_TYPE_CODE,
+                    redirectUri
+            );
+            builder.setScopes("profile");
+            AuthorizationRequest request = builder.build();
+
+            AuthorizationService authorizationService = new AuthorizationService(view.getContext());
+
+            String action = "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE";
+            Intent postAuthorizationIntent = new Intent(action);
+            PendingIntent pendingIntent = PendingIntent.getActivity(view.getContext(), request.hashCode(), postAuthorizationIntent, 0);
+            authorizationService.performAuthorizationRequest(request, pendingIntent);
+
+        }
+    }
+
+    public static class SignOutListener implements Button.OnClickListener {
+
+        private final MainActivity mMainActivity;
+
+        public SignOutListener(@NonNull MainActivity mainActivity) {
+            mMainActivity = mainActivity;
+        }
+
+        @Override
+        public void onClick(View view) {
+            mMainActivity.mAuthState = null;
+            mMainActivity.clearAuthState();
+            mMainActivity.enablePostAuthorizationFlows();
+        }
+    }
+
+    public static class MakeApiCallListener implements Button.OnClickListener {
+
+        private final MainActivity mMainActivity;
+        private AuthState mAuthState;
+        private AuthorizationService mAuthorizationService;
+
+        public MakeApiCallListener(@NonNull MainActivity mainActivity, @NonNull AuthState authState, @NonNull AuthorizationService authorizationService) {
+            mMainActivity = mainActivity;
+            mAuthState = authState;
+            mAuthorizationService = authorizationService;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            // code from the section 'Making API Calls' goes here
+
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        checkIntent(intent);
+    }
+
+    private void checkIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            switch (action) {
+                case "com.google.codelabs.appauth.HANDLE_AUTHORIZATION_RESPONSE":
+                    if (!intent.hasExtra(USED_INTENT)) {
+                        handleAuthorizationResponse(intent);
+                        intent.putExtra(USED_INTENT, true);
+                    }
+                    break;
+                default:
+                    // do nothing
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkIntent(getIntent());
+    }
+}
---
+++ app/src/main/java/io/github/mikbuch/appauthtest/MainApplication.java
@@ -0,0 +1,13 @@
+package io.github.mikbuch.appauthtest;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+  public static final String LOG_TAG = "AppAuthSample";
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
